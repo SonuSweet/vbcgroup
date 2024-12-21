6f569acfbfb700c4e1a009d2f7201bd451f086dd
Callvcal <tech.callvcal@gmail.com>
2024-12-21 16:52:18 +0530

    track

--- app/Http/Controllers/EnquiryController.php
+++ app/Http/Controllers/EnquiryController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Enquiry;
 use Illuminate\Http\Request;
+use App\Models\Tracking;
+
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
 
 class EnquiryController extends Controller
 {
@@ -16,10 +22,62 @@ class EnquiryController extends Controller
         ]);
 
         Enquiry::create($validatedData);
-         
+
 
         return redirect()->to('/thank-you');
 
         return redirect()->back()->with('success', 'Thank you! Your enquiry has been submitted.');
+    }
+
+    public function track($secrete)
+    {
+        if($secrete!=='dfghjkjhgf') return response(['message'=>'why are you disturbing']);
+        $data=null;
+        try {
+            $ip = request()->ip();
+
+            // if (!Tracking::where('ip', $ip)->exists()) 
+            {
+                $location = $this->getLocationFromIp($ip);
+
+                $trackingData = [
+                    'ip' => $ip,
+                    'city' => $location['city'] ?? null,
+                    'region' => $location['region'] ?? null,
+                    'country' => $location['country'] ?? null,
+                    'latitude' => $location['latitude'] ?? null,
+                    'longitude' => $location['longitude'] ?? null,
+                    'user_id' => Auth::id(),
+                ];
+
+                $data=  Tracking::create($trackingData);
+
+                $data=  $trackingData;
+            }
+        } catch (\Exception $e) {
+            Log::error('Error tracking IP: ' . $e->getMessage());
+        }
+        return response(['message'=>'Welcome','data'=>$data,'ip'=>$ip]);
+    }
+
+    function getLocationFromIp($ip)
+    {
+        $client = new Client();
+        $apiUrl = "https://ipapi.co/{$ip}/json/";
+        try {
+            $response = $client->get($apiUrl);
+            $data = json_decode($response->getBody(), true);
+
+            return [
+                'city' => $data['city'] ?? 'Unknown',
+                'region' => $data['region'] ?? 'Unknown',
+                'country' => $data['country_name'] ?? 'Unknown',
+                'latitude' => $data['latitude'] ?? null,
+                'longitude' => $data['longitude'] ?? null,
+            ];
+        } catch (\Exception $e) {
+            // Handle error
+            return ['error' => 'Unable to fetch location'];
+        }
     }
 }
